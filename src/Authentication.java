aa9c689d0a3aecb708ceb4dc544639118a5aa0a3
meganathan <ramprasathmeganathan@Rams-MacBook-Pro.local>
2020-11-28 00:48:49 -0400

    Authentication.java

---
+++ src/Authentication.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+public class start {
+    String credentialsFile = "credentials.csv";
+    String credential = "";
+    public void register(String username, String password) throws IOException {
+        credential = "";
+        File file = new File(credentialsFile);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        BufferedReader bufferedReaderObject = new BufferedReader(new FileReader(credentialsFile));
+        while ((credential = bufferedReaderObject.readLine()) != null) {
+            String[] credentials = credential.split(",");
+            if (credentials[0] == username && credentials[1] == password) {
+                System.out.println("User Already Registered");
+            } else {
+                BufferedWriter bufferedWriterObject = new BufferedWriter(new FileWriter(credentialsFile, true));
+                bufferedWriterObject.write(username + "," + password + "\n");
+                bufferedWriterObject.close();
+                System.out.println("User Registered successfully");
+            }
+        }
+    }
+    public void authenticate(String username, String password) throws IOException
+    {
+
+        BufferedReader bufferedReaderObject = new BufferedReader(new FileReader(credentialsFile));
+        while((credential = bufferedReaderObject.readLine())!=null){
+            String[] credentials = credential.split(",");
+            if(credentials[0]==username&&credentials[1]==password)
+            {
+                System.out.println("Authenticated");
+            }
+            else
+            {
+                System.out.println("Not Authenticated");
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        start testObject = new start();
+        Scanner scannerObject = new Scanner(System.in);
+        System.out.println("operations available 1. registration 2. authentication ");
+        System.out.println("Enter your choice ");
+        int choice = scannerObject.nextInt();
+        System.out.println("Enter the username");
+        String username = scannerObject.next();
+        System.out.println("Enter the password");
+        String password = scannerObject.next();
+        switch(choice){
+            case 1:
+                testObject.register(username,password);
+                break;
+            case 2:
+                testObject.authenticate(username,password);
+                break;
+            default:
+                System.out.println("invalid choice");
+        }
+
+
+//https://stackabuse.com/reading-and-writing-csvs-in-java/
+
+    }
+}
+

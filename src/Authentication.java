b7881327e5ba5860c4d1f07d98dec910c8f8d65e
meganathan <ramprasathmeganathan@Rams-MacBook-Pro.local>
2020-11-29 18:57:48 -0400

    correct infinite loop bug on authentication

--- src/Authentication.java
+++ src/Authentication.java
@@ -1,17 +1,15 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
 import java.util.Scanner;
 
-public class Authentication {
-	String credentialsFile = "/Users/manpreetsingh/Documents/dataproject5408/src/credentials.csv";
-	String credential = "";
 
+
+public class start {
+	String credentialsFile = "credentials.csv";
+	String credential = "";
 	public void register(String username, String password) throws IOException {
 		credential = "";
+		boolean isRegistered=false;
 		File file = new File(credentialsFile);
 		if (!file.exists()) {
 			file.createNewFile();
@@ -19,59 +17,68 @@ public class Authentication {
 		BufferedReader bufferedReaderObject = new BufferedReader(new FileReader(credentialsFile));
 		while ((credential = bufferedReaderObject.readLine()) != null) {
 			String[] credentials = credential.split(",");
-			if (credentials[0] == username && credentials[1] == password) {
+			if (credentials[0].toLowerCase().equals(username.toLowerCase())) {
 				System.out.println("User Already Registered");
-			} else {
-				BufferedWriter bufferedWriterObject = new BufferedWriter(new FileWriter(credentialsFile, true));
-				bufferedWriterObject.write(username + "," + password + "\n");
-				bufferedWriterObject.close();
-				System.out.println("User Registered successfully");
+				isRegistered=true;
+				break;
 			}
+		}
+		if(!isRegistered) {
+			BufferedWriter bufferedWriterObject = new BufferedWriter(new FileWriter(credentialsFile, true));
+			bufferedWriterObject.write(username + "," + password + "\n");
+			bufferedWriterObject.close();
+			System.out.println("User Registered successfully");
 		}
 	}
-
-	public void authenticate(String username, String password) throws IOException {
-
+	public void authenticate(String username, String password) throws IOException
+	{
 		BufferedReader bufferedReaderObject = new BufferedReader(new FileReader(credentialsFile));
-		while ((credential = bufferedReaderObject.readLine()) != null) {
+		while((credential = bufferedReaderObject.readLine())!=null){
 			String[] credentials = credential.split(",");
-			if (credentials[0].equals(username) && credentials[1].equals(password)) {
-				System.out.println("Authenticated");
-				QueryInit q1=new QueryInit();
-				q1.init();
-			} else {
-				System.out.println("Not Authenticated");
+			if(credentials!=null) {
+				if(credentials[0]!=""||credentials[1]!="") {
+					if (credentials[0].toLowerCase() == username.toLowerCase() && credentials[1] == password) {
+						System.out.println("Authenticated");
+					} else {
+						System.out.println("Not Authenticated");
+					}
+				}
 			}
 		}
-
 	}
 
 	public static void main(String[] args) throws IOException {
-		Authentication testObject = new Authentication();
+		start testObject = new start();
+		DumpCreation dumpCreationObject = new DumpCreation();
 		Scanner scannerObject = new Scanner(System.in);
-		//DumpCreation dumpCreationObject=new DumpCreation();
-		System.out.println("operations available 1. registration 2. authentication 3.DB Dump Creation");
+		System.out.println("operations available 1. registration 2. authentication 3.DB Dump Creation 4. ERD Generation");
 		System.out.println("Enter your choice ");
 		int choice = scannerObject.nextInt();
 		System.out.println("Enter the username");
 		String username = scannerObject.next();
 		System.out.println("Enter the password");
 		String password = scannerObject.next();
-		switch (choice) {
-		case 1:
-			testObject.register(username, password);
-			break;
-		case 2:
-			testObject.authenticate(username, password);
-			break;
-		case 3:
-			dumpCreationObject.CreateDump("firstDb");
-			break;
-		default:
-			System.out.println("invalid choice");
+		switch(choice){
+			case 1:
+				testObject.register(username,password);
+				break;
+			case 2:
+				testObject.authenticate(username,password);
+				break;
+			case 3:
+				dumpCreationObject.CreateDump("firstDb");
+				break;
+			case 4:
+				Process p = Runtime.getRuntime().exec("python3 ERDExample.py");
+				System.out.println("Database file generated");
+				break;
+			default:
+				System.out.println("invalid choice");
 		}
+
 
 //https://stackabuse.com/reading-and-writing-csvs-in-java/
 
 	}
 }
+
